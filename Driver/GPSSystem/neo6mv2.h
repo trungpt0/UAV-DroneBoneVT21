fbefb0b98c79c3565e15b9f31d76c1cd17a82adc
trungpt0 <trungtrnminh368@gmail.com>
2024-04-22 23:09:03 +0700

    Update Neo6M

--- Driver/GPSSystem/neo6mv2.h
+++ Driver/GPSSystem/neo6mv2.h
@@ -7,14 +7,4 @@
 
 #include <unistd.h>
 
-#define GPS_DEVICE "/dev/ttyS1" // Path to the GPS UART device
-
-/**
- * Prototypes in neo6mv2.c
-*/
-int openGPS();
-void closeGPS(int fd);
-void configureUART(int fd);
-int readGPS(int fd, char *buffer, size_t size);
-
 #endif /* NEO_H */
