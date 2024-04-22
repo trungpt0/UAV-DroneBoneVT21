169d46fdabcde6f197f8f607272922943db97e2a
trungpt0 <trungtrnminh368@gmail.com>
2024-04-22 09:53:19 +0700

    MPU6050 Update

--- Src/main.c
+++ Src/main.c
@@ -26,6 +26,65 @@
 
 int fd;
 
+/* The MPU6050 processing functions */
+int mpu6050_write(uint8_t addr, uint8_t data);
+int mpu6050_read(uint8_t base_addr, char *pbuffer, int len);
+void mpu6050_init();
+void mpu6050_read_acc(short int *pbuffer);
+void mpu6050_read_gyr(short int *pbuffer);
+
+int main()
+{
+	short acc_value[3];
+	short gyr_value[3];
+	double accx, accy, accz, gyrx, gyry, gyrz;
+
+	/* Open I2C device file */
+	if ((fd = open(I2C_DEVICE_FILE, O_RDWR)) < 0) {
+		perror("Failed to open I2C device file.\n");
+    	return -1;
+	}
+
+	/* Set I2C slave address */
+	if (ioctl(fd, I2C_SLAVE, MPU6050_SLAVE_ADDR) < 0) {
+		perror("Failed to set I2C slave address.\n");
+		close(fd);
+		return -1;
+	}
+
+	/* Initialize MPU6050 */
+	mpu6050_init();
+
+	while (1)
+	{
+		/* Read accelerometer and gyroscope data */
+		mpu6050_read_acc(acc_value);
+		mpu6050_read_gyr(gyr_value);
+
+		/* Convert raw data to actual values */
+		accx = (double) acc_value[0]/AFS_SEL_0;
+		accy = (double) acc_value[1]/AFS_SEL_0;
+		accz = (double) acc_value[2]/AFS_SEL_0;
+
+		gyrx = (double) gyr_value[0]/GFS_SEL_0;
+		gyry = (double) gyr_value[1]/GFS_SEL_0;
+		gyrz = (double) gyr_value[2]/GFS_SEL_0;
+
+		printf("Acc(raw) = X:%d   Y:%d   Z:%d, Gyr(raw) = X:%d   Y:%d   Z:%d\n", 
+			acc_value[0], acc_value[1], acc_value[2],
+			gyr_value[0], gyr_value[1], gyr_value[2]
+		);
+
+		printf("Acc(g) = X:%.2f   Y:%.2f   Z:%.2f, Gyr(g) = X:%.2f   Y:%.2f   Z:%.2f\n", 
+			accx, accy, accz,
+			gyrx, gyry, gyrz
+		);
+
+		usleep(50 * 1000);
+	}
+	return 0;
+}
+
 /* Function to write data to MPU6050 */
 int mpu6050_write(uint8_t addr, uint8_t data)
 {
@@ -92,56 +151,4 @@ void mpu6050_read_gyr(short int *pbuffer)
 	pbuffer[0] = (int) ((gyr_buffer[0] << 8) + gyr_buffer[1]);
 	pbuffer[1] = (int) ((gyr_buffer[2] << 8) + gyr_buffer[3]);
 	pbuffer[2] = (int) ((gyr_buffer[4] << 8) + gyr_buffer[5]);
-}
-
-int main()
-{
-	short acc_value[3];
-	short gyr_value[3];
-	double accx, accy, accz, gyrx, gyry, gyrz;
-
-	/* Open I2C device file */
-	if ((fd = open(I2C_DEVICE_FILE, O_RDWR)) < 0) {
-		perror("Failed to open I2C device file.\n");
-    	return -1;
-	}
-
-	/* Set I2C slave address */
-	if (ioctl(fd, I2C_SLAVE, MPU6050_SLAVE_ADDR) < 0) {
-		perror("Failed to set I2C slave address.\n");
-		close(fd);
-		return -1;
-	}
-
-	/* Initialize MPU6050 */
-	mpu6050_init();
-
-	while (1)
-	{
-		/* Read accelerometer and gyroscope data */
-		mpu6050_read_acc(acc_value);
-		mpu6050_read_gyr(gyr_value);
-
-		/* Convert raw data to actual values */
-		accx = (double) acc_value[0]/AFS_SEL_0;
-		accy = (double) acc_value[1]/AFS_SEL_0;
-		accz = (double) acc_value[2]/AFS_SEL_0;
-
-		gyrx = (double) gyr_value[0]/GFS_SEL_0;
-		gyry = (double) gyr_value[1]/GFS_SEL_0;
-		gyrz = (double) gyr_value[2]/GFS_SEL_0;
-
-		printf("Acc(raw) = X:%d   Y:%d   Z:%d, Gyr(raw) = X:%d   Y:%d   Z:%d\n", 
-			acc_value[0], acc_value[1], acc_value[2],
-			gyr_value[0], gyr_value[1], gyr_value[2]
-		);
-
-		printf("Acc(g) = X:%.2f   Y:%.2f   Z:%.2f, Gyr(g) = X:%.2f   Y:%.2f   Z:%.2f\n", 
-			accx, accy, accz,
-			gyrx, gyry, gyrz
-		);
-
-		usleep(50 * 1000);
-	}
-	return 0;
 }
